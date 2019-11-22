4999c9a24488d854cb58df9aaa8da68909922879
grzdrz <spaceofgrz@gmail.com>
2019-11-22 15:55:59 +0700

    Commit_0: Worked project

---
+++ AlphaChannelInsertion/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlphaChannelInsertion
+{
+    public partial class Form1 : Form
+    {
+        public Bitmap bitmap1;
+        public Bitmap bitmap2;
+        public Bitmap bitmap3;
+
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += button1_Click;
+            button2.Click += button2_Click;
+            button3.Click += button3_Click;            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.CheckFileExists = true;
+            openFileDialog.AddExtension = true;
+            openFileDialog.Filter = "PNG files (*.png)|*.png";
+
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                bitmap1 = (Bitmap)Bitmap.FromFile(openFileDialog.FileName);
+                pictureBox1.Image = bitmap1;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.CheckFileExists = true;
+            openFileDialog.AddExtension = true;
+            openFileDialog.Filter = "PNG files (*.png)|*.png";
+
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                bitmap2 = (Bitmap)Bitmap.FromFile(openFileDialog.FileName);
+                pictureBox2.Image = bitmap2;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Insertion insertion = new Insertion(bitmap1, bitmap2);
+            bitmap3 = insertion.Insert();
+            pictureBox3.Image = bitmap3;
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.CheckFileExists = false;
+            saveFileDialog.AddExtension = true;
+            saveFileDialog.Filter = "PNG files (*.png)|*.png";
+
+            DialogResult showDialogResult = saveFileDialog.ShowDialog(); 
+            if (showDialogResult == DialogResult.Cancel)
+                return;
+            if (showDialogResult == System.Windows.Forms.DialogResult.OK)
+                using (FileStream stream = new FileStream(saveFileDialog.FileName, FileMode.Create))
+                {
+                    bitmap3.Save(stream, ImageFormat.Png);
+                }
+            MessageBox.Show("Файл сохранен");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+//Bitmap.FromFile статический метод из базового абстрактного класса Image, создающий экземпляр наследника Bitmap и апкастящий его до Image.
+//Поэтому чтобы получить Bitmap экземпляр с помощью этого метода нужно после его вызова даункастнуть объект до Bitmap.
---
+++ AlphaChannelInsertion/Insertion.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlphaChannelInsertion
+{
+    public class Insertion
+    {
+        public Bitmap bitmap1;
+        public Bitmap bitmap2;
+        public Bitmap bitmap3;
+        public Color[,] pixelsArray1;
+        public Color[,] pixelsArray2;
+        public Color[,] pixelsArray3;
+
+        public Insertion(Bitmap bitmap1, Bitmap bitmap2)
+        {
+            this.bitmap1 = bitmap1;
+            this.bitmap2 = bitmap2;
+            this.bitmap3 = new Bitmap(bitmap1.Width, bitmap1.Height);
+        }
+
+        public Bitmap Insert()
+        {
+            pixelsArray1 = new Color[bitmap1.Width, bitmap1.Height];
+            pixelsArray2 = new Color[bitmap2.Width, bitmap2.Height];
+            pixelsArray3 = new Color[bitmap1.Width, bitmap1.Height];
+
+            int alpha;
+            for (int i = 0; i < pixelsArray1.GetLength(0); i++)
+                for (int j = 0; j < pixelsArray1.GetLength(1); j++)
+                {
+                    pixelsArray1[i, j] = bitmap1.GetPixel(i, j);
+                    pixelsArray2[i, j] = bitmap2.GetPixel(i, j);
+
+                    alpha = (int)((pixelsArray2[i, j].B + pixelsArray2[i, j].R + pixelsArray2[i, j].G) / 3);
+                    pixelsArray3[i, j] = Color.FromArgb(alpha, pixelsArray1[i, j]);
+                    bitmap3.SetPixel(i, j, pixelsArray3[i, j]);
+                }
+            return bitmap3;
+        }
+    }
+}
+//0   0   0   - черный
+//255 255 255 - белый
+//n   n   n   - серые, при 0 < n < 255 
+
+//Черный цвет -> альфа-канал пикселя -> прозрачный пиксель
+//Белый цвет -> альфа-канал пикселя -> непрозрачный пиксель
